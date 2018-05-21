29d3a8a666d4db408cecc0383fafbc3c0197d3d5
Courtney Adams <cadams8238@gmail.com>
2018-05-21 12:55:08 -0700

    added get api/notes functionality

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 'use strict';
 
 // Load array of notes
+// const data = require('./db/notes');
+console.log('Hello Noteful!');
+const express = require('express');
+const app = express();
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+// ADD STATIC SERVER HERE
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+	res.json(data);
+});
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
