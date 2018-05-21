7e8918aad8381b5f7f9b5835cc915cc183c262b1
Courtney Adams <cadams8238@gmail.com>
2018-05-21 13:38:33 -0700

    implemented get request for individual notes

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.get('/api/notes', (req, res) => {
 	res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+	console.log(req.params);
+	const findNoteById = data.find(note => note.id === Number(req.params.id)); 
+	res.json(findNoteById);
+});
+
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
