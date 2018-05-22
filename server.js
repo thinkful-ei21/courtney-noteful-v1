7f2a00758e4f218f5ddc78cf34f091118d85cfc0
Courtney Adams <cadams8238@gmail.com>
2018-05-22 11:42:21 -0700

    added module for PORT

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ console.log('Hello Noteful!');
 const express = require('express');
 const app = express();
 const data = require('./db/notes');
+const { PORT } = require('./config');
 
 
 // ADD STATIC SERVER HERE
@@ -30,7 +31,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
