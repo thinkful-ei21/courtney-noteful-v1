7036f7204c83206dbdbbec56adbfdd19e36ad887
Courtney Adams <cadams8238@gmail.com>
2018-05-23 13:05:54 -0700

    added morgan in requestLogger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const morgan = require('morgan');
 
-function requestLogger(req, res, next) {
-	const today = new Date();
-	console.log(`${today.toLocaleDateString()} ${today.toLocaleTimeString()} ${req.method} ${req.url}`);
-	next();
-}
 
+// function requestLogger(req, res, next) {
+// 	const today = new Date();
+// 	console.log(`${today.toLocaleDateString()} ${today.toLocaleTimeString()} ${req.method} ${req.url}`);
+// 	next();
+// }
+
+const requestLogger = morgan('dev');
 
 module.exports = {requestLogger};
 
